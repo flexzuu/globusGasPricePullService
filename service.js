8d26c2afe30202b06963ca2c1e636373f8601462
Jonas Faber <dev.jonas.faber@gmail.com>
2016-09-30 11:53:53 +0200

    Change cron pattern to full hour

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
 import { CronJob } from 'cron';
 import task from './pullGlobus';
 const job = new CronJob({
-  cronTime: '*/10 * * * * *',
+  cronTime: '0 0 * * * *',
   onTick: task,
 });
 job.start();
