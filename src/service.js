0a3732a3b469eccdcf43e32333f5d34d111fd169
Jonas Faber <dev.jonas.faber@gmail.com>
2016-10-04 13:23:03 +0200

    Interval every minute to keep alive.

--- src/service.js
+++ src/service.js
@@ -2,5 +2,14 @@ import task from './pullGlobus';
 //Initial run ...
 task();
 //Run every time milliseconds ...
-const time = 1000* 60* 30;
-setInterval(task, time);
+const time = 1000* 60;
+let timer = 30;
+setInterval(()=>{
+  console.log(new Date().toString(), "Running...");
+  if(timer == 0){
+    task();
+    timer = 30;
+  }else {
+    timer--;
+  }
+}, time);
