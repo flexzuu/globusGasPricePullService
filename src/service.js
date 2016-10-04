a43dba7cf0ce36e149b5892fce665a9c606e8dd3
Jonas Faber <dev.jonas.faber@gmail.com>
2016-10-04 13:29:22 +0200

    Better logging

--- src/service.js
+++ src/service.js
@@ -1,11 +1,12 @@
 import task from './pullGlobus';
 //Initial run ...
+console.log(new Date().toString(), "Initial run");
 task();
 //Run every time milliseconds ...
 const time = 1000* 60;
 let timer = 30;
 setInterval(()=>{
-  console.log(new Date().toString(), "Running...");
+  console.log(new Date().toString(), "Running...", "Timer:", timer);
   if(timer == 0){
     task();
     timer = 30;
